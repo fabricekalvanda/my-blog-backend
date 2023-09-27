be3ce4e6650d6c0ab84fd41b997b90036958899c
FabriceKalvanda <67396845+fabricekalvanda@users.noreply.github.com>
2023-09-27 11:41:31 -0700

    adds functionallity to upvote and comment article

--- src/server.js
+++ src/server.js
@@ -1,11 +1,60 @@
 import express from "express";
 
-const app = express();
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+        comments: [],
+    }, 
+    {
+        name: 'learn-node',
+        upvotes: 0,
+        comments: [],
+    }, 
+    {
+        name: 'mongodb',
+        upvotes: 0,
+        comments: [],
+    }
+]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+const app = express();
+app.use(express.json());
+
+/* app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
+});
+
+app.get('/hello/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
+}) */
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article){
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvote(s)!!`);
+    } else {
+        res.send(`That article doesn't exist`);
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    }else {
+        res.send(`That article doesn't exist`);
+    }
+    
 });
 
 app.listen(8000, () => {
     console.log('Server listening on port 8000');
-})
+});
