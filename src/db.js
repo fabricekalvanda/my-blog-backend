896f1d54215337ec814bb0eb4ed974ded08b1254
fabricekalvanda <fabricekalvanda@gmail.com>
2023-12-13 09:37:26 -0800

    host mongoDB

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb";
 let db;
 
 async function connectToDB(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017'); // connect to local mongodb
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ghldva8.mongodb.net/?retryWrites=true&w=majority`); // connect to local mongodb
     await client.connect();
     db = client.db('react-blog-db'); // reeferencing the specific database
     cb();
